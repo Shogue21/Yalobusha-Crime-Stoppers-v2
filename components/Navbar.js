86ee1f8034bf680a0a0f226b584b9b6905b0a6b4
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-07-12 15:47:45 -0500

    Commit message

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,13 @@
+import styles from '../styles/navbar.module.css'
+
 const Navbar = () => {
     return (
-        <nav>
-            <h3>Yalobusha County Crime Stoppers</h3>
-            <a href="/about">About Me</a>
-            <a href="/tip">Submit a Tip</a>
+        <nav className={styles.nav}>
+            <h3 className={styles.header}>Yalobusha County Crime Stoppers</h3>
+            <div className={styles.links}>
+                <a className={styles.link} href="/about">About Us</a>
+                <a className={styles.link} href="/tip">Submit a Tip</a>
+            </div>
         </nav>
     )
 }
