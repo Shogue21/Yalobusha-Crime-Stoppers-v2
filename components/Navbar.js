7afed1f7fe7217a09beb5cd1e0824f3b446f9d92
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-07-13 09:08:05 -0500

    Navbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,9 +3,10 @@ import styles from '../styles/navbar.module.css'
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <h3 className={styles.header}>Yalobusha County Crime Stoppers</h3>
+            <a href="/" className={styles.header}><h3>Yalobusha County Crime Stoppers</h3></a>
             <div className={styles.links}>
                 <a className={styles.link} href="/about">About Us</a>
+                <a className={styles.link} href="/contact">Contact Us</a>
                 <a className={styles.link} href="/tip">Submit a Tip</a>
             </div>
         </nav>
