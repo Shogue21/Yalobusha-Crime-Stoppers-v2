5dde92072b544b2b46fa19ed68c22e7d37955284
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-07-13 15:10:31 -0500

    Info for about page - styling for about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 
+import styles from '../styles/about.module.css'
+
 export default function AboutUs() {
     return (
-        <div className="container">
+        <div className={styles.container}>
        <Head>
          <title>About Us</title>
          <link rel="icon" href="/favicon.ico" />
@@ -10,53 +12,56 @@ export default function AboutUs() {
 
     <body>
       <div className="about-us">
-          <h1 >About Us</h1>
+          <h1 className={styles.h1}>About Us</h1>
       </div>
-      <div class="content">
-      <p>
-      Yalobusha County Crime Stoppers
-      encourages members of the community to
-      assist local law enforcement agencies in the
-      fight against crime by overcoming the two key
-      elements that inhibit community involvement -
-      fear and apathy.  Crime Stoppers provides a
-      telephone number and Web Tips to encourage
-      citizens in the community to volunteer vital
-      information helpful to law enforcement
-      agencies to fight against crime.  Callers
-      remain anonymous and are eligible to receive
-      a cash reward if the information given leads to
-      an arrest or grand jury indictment of a felony
-      offender.
-      </p>
-      
-      <p>
-      The Yalobusha County Crime Stoppers “hotline”
-      is a vital resource in helping reduce crime in our
-      community. With its triangle of cooperation
-      between the citizens, the media and law
-      enforcement, Yalobusha County Crime Stoppers
-      is dedicated to putting criminals behind bars
-      while building a foundation of trust and support.
-      The goal of Yalobusha County Crime Stoppers is
-      to make the streets safer for our families and
-      neighborhoods.
-      </p>
-
-      <p>
-      Since the inception of our local program in 2006
-      the “TIPS Hotline,” as it is more commonly
-      known, has taken numerous tips from concerned
-      citizens. Those tips have aided in the arrests of
-      several criminals and the clearing of recent some
-      felony cases or warrants. Since it is a well-
-      documented fact that criminals tend to keep
-      committing crimes, the sum total of arrests from
-      Crime Stoppers tips has undoubtedly prevented
-      other crimes from occurring.
-      </p>
+      <div className={styles.AboutUsContent}>
+        <p>
+        Yalobusha County Crime Stoppers
+        encourages members of the community to
+        assist local law enforcement agencies in the
+        fight against crime by overcoming the two key
+        elements that inhibit community involvement -
+        fear and apathy.  Crime Stoppers provides a
+        telephone number and Web Tips to encourage
+        citizens in the community to volunteer vital
+        information helpful to law enforcement
+        agencies to fight against crime.  Callers
+        remain anonymous and are eligible to receive
+        a cash reward if the information given leads to
+        an arrest or grand jury indictment of a felony
+        offender.
+        </p>
+        
+        <p>
+        The Yalobusha County Crime Stoppers “hotline”
+        is a vital resource in helping reduce crime in our
+        community. With its triangle of cooperation
+        between the citizens, the media and law
+        enforcement, Yalobusha County Crime Stoppers
+        is dedicated to putting criminals behind bars
+        while building a foundation of trust and support.
+        The goal of Yalobusha County Crime Stoppers is
+        to make the streets safer for our families and
+        neighborhoods.
+        </p>
       </div>
       
+      <div className={styles.tipProcess}>
+        <h1 className={styles.h1}>Tip Process</h1>
+        <h3 className={styles.howTo}>How do I submit a tip? </h3>
+        <p>
+        First you should know that you remain completley anonymous and are eligible to receive a cash reward if the information given leads to an arrest or grand jury indictment of a felony offender.
+        Second you should know that you can submit a tip to Yalobusha County Crime Stoppers by calling the tip line at bottom of the page. 
+        </p>
+      </div>
+      <div className={styles.community}>
+      <h1 className={styles.h1}>
+        Why should I call?
+      </h1>
+        <p>
+          It can help to protect you, your loved ones and community from future crimes
+        </p>
+      </div>
     </body>
     </div>
     )
