3c79c1edb26324bb669a434aa6c61e84b5d778d7
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-07-13 15:28:24 -0500

    index page button fixed and styled

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import styles from '../styles/home.module.css'
+import { useState } from 'react'
 
 const index = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  const buttonHovered = () => {
+    return <a className={styles.transparent_btn_Hover} href="/about" onMouseLeave={() => {setIsHover(false)}} >Learn More</a>
+  }
+  const buttonNotHovered = () => {
+    return <a className={styles.button} href="/about" onMouseOver={() => {setIsHover(true)}}>Learn More</a>
+  }
+
   return (
     <div className={styles.landing}>
       <div className={styles.center}>
         <h1 className={styles.text}>Yalobusha County Crime Stoppers</h1>
         <p className={styles.text}>If you see something, or hear something, say something!</p>
         <div className={styles.buttons}>
-          <a className={styles.button} href="/about"><button className={styles.buttonSize}>Learn More</button></a>
+          {isHover && buttonHovered()}
+          {!isHover && buttonNotHovered()}
         </div>
       </div>
     </div>
