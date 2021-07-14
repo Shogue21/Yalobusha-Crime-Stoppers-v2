4697d8ed5afbc4a881c32ec000ce67a34ef55bd9
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-07-14 10:46:35 -0500

    updated navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,14 +9,24 @@ const Navbar = () => {
         }); 
       }; 
     return (
-        <nav className={styles.nav}>
-            <div className={styles.links}>
-                <Link href="/"><a className={styles.noDecor}><img className={styles.logo} src="logo.gif"></img></a></Link>
-                <Link href="/" className={styles.header}><a className={styles.noDecor}><h3>Yalobusha County Crime Stoppers</h3></a></Link>
-            </div>
-            <div className={styles.links}>
-                <Link className={styles.link} href="/about"><a className={styles.noDecor}>About</a></Link>
-                <a className={`${styles.link} ${styles.noDecor}`} onClick={scrollToBottom}>Contact</a>
+        
+        <nav className="navbar navbar-expand-lg bg-light navbar-light">
+            <div className="container-fluid justify-content-between">
+                <div className="d-flex align-items-center">
+                    <Link href="/"><a className={styles.noDecor}><img className={styles.logo} src="logo.gif"></img></a></Link>
+                    <Link href="/"><a className={`${styles.noDecor} ${styles.header} align-middle`}>Yalobusha County Crime Stoppers</a></Link>
+                </div>
+                <div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link href="/about"><a className={`${styles.link}`} aria-current="page">About</a></Link>
+                        <a className={`${styles.link}`} onClick={scrollToBottom}>Contact</a>
+                    </div>
+                    </div>
+                </div>
             </div>
         </nav>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/styles.css'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
 
 
 
@@ -9,10 +11,9 @@ export default function MyApp({ Component, pageProps }) {
   return <div>
   <Head> 
     <title>Yalobusha County Crime Stoppers</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
     <meta name="HandheldFriendly" content="true"></meta>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous"></script>
   </Head>
   
   <Navbar />
