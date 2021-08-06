f510bf222a4acd4604f8fa06490be8e767232255
Seth Hogue <shogue21@basecampcodingacademy.org>
2021-08-06 09:52:23 -0500

    added sorting for table

--- pages/tips.tsx
+++ pages/tips.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "next-auth/client"
 import { Prisma, PrismaClient, Tip } from "@prisma/client";
 import styles from '../styles/tips.module.css'
+import { useState } from "react";
 
 const prisma = new PrismaClient();
 
@@ -16,6 +17,92 @@ export async function getServerSideProps(ctx) {
   }
 
 const tips = ({session, allTips}) => {
+    const [sorted, setSorted] = useState(false);
+    const [sortedReverse, setSortedReverse] = useState(false);
+
+    const sortTable = (column) => {
+        if ((!sorted && !sortedReverse) || (!sorted && sortedReverse)) {
+            setSorted(true);
+            setSortedReverse(false);
+            allTips.sort(function(a, b) {
+                var itemA;
+                var itemB;
+                switch (column) {
+                    case "offenseType":
+                        itemA = a.offenseType.toUpperCase();
+                        itemB = b.offenseType.toUpperCase();
+                        break;
+                    case "address":
+                        itemA = a.address.toUpperCase();
+                        itemB = b.address.toUpperCase();
+                        break;
+                    case "nearestIntersection":
+                        itemA = a.nearestIntersection.toUpperCase();
+                        itemB = b.nearestIntersection.toUpperCase();
+                        break;
+                    case "neighborhood":
+                        itemA = a.neighborhood.toUpperCase();
+                        itemB = b.neighborhood.toUpperCase();
+                        break;
+                    case "arrested":
+                        itemA = a.arrested;
+                        itemB = b.arrested;
+                        if (itemA == false) {
+                            itemA = "No"
+                        } else if (itemA == true) {
+                            itemA = "Yes"
+                        } else if (itemB == false) {
+                            itemB = "No"
+                        } else if (itemB == true) {
+                            itemB = "Yes"
+                        }
+                        break;
+                }
+                return itemA.localeCompare(itemB);
+              });
+        } else if (sorted && !sortedReverse) {
+            setSorted(false);
+            setSortedReverse(true);
+            allTips.sort(function(a, b) {
+                var itemA;
+                var itemB;
+                switch (column) {
+                    case "offenseType":
+                        itemA = a.offenseType.toUpperCase();
+                        itemB = b.offenseType.toUpperCase();
+                        break;
+                    case "address":
+                        itemA = a.address.toUpperCase();
+                        itemB = b.address.toUpperCase();
+                        break;
+                    case "nearestIntersection":
+                        itemA = a.nearestIntersection.toUpperCase();
+                        itemB = b.nearestIntersection.toUpperCase();
+                        break;
+                    case "neighborhood":
+                        itemA = a.neighborhood.toUpperCase();
+                        itemB = b.neighborhood.toUpperCase();
+                        break;
+                    case "arrested":
+                        itemA = a.arrested;
+                        itemB = b.arrested;
+                        if (itemA == false) {
+                            itemA = "No"
+                        } else if (itemA == true) {
+                            itemA = "Yes"
+                        } else if (itemB == false) {
+                            itemB = "No"
+                        } else if (itemB == true) {
+                            itemB = "Yes"
+                        }
+                        break;
+                }
+                return itemA.localeCompare(itemB)
+              }).reverse();
+        }
+        return allTips;
+    }
+
     const renderTips = () => {
         return allTips.map(tip => 
             <tr key={tip.id}>
@@ -32,15 +119,30 @@ const tips = ({session, allTips}) => {
     if (session) {
             return (
                 <div className={`${styles.body} table-responsive`}>
-                    <table className="table table-sm table-striped table-hover">
+                    <table className="table table-sm table-striped table-hover sortable">
                         <thead>
                             <tr>
                             <th scope="col">Description</th>
-                            <th scope="col">Offense Type</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Nearest Intersection</th>
-                            <th scope="col">Neighborhood</th>
-                            <th scope="col">Arrested?</th>
+                            <th onClick={() => {
+                                allTips = sortTable('offenseType')
+                                console.log("offense")
+                                }} scope="col">Offense Type</th>
+                            <th onClick={() => {
+                                allTips = sortTable('address')
+                                console.log("address")
+                                }} scope="col">Address</th>
+                            <th onClick={() => {
+                                allTips = sortTable('nearestIntersection')
+                                console.log("intersection")
+                                }} scope="col">Nearest Intersection</th>
+                            <th onClick={() => {
+                                allTips = sortTable('neighborhood')
+                                console.log("neighborhood")
+                                }} scope="col">Neighborhood</th>
+                            <th onClick={() => {
+                                allTips = sortTable('arrested')
+                                console.log("arrested")
+                                }} scope="col">Arrested?</th>
                             </tr>
                         </thead>
                         <tbody>
