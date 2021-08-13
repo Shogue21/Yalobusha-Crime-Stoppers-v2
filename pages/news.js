00f3cfb74baad5cd2f02a85ef56aff051518f315
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-08-13 08:32:51 -0500

    news target blank - stats pie chart changes

--- pages/news.js
+++ pages/news.js
@@ -20,7 +20,7 @@ export const News = ({ articles }) => {
             <div className={styles.main}>
                 {articles.map((article, index) => (
                     <div key={index} className={styles.post}>
-                        <h1 className={styles.postH1} onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+                        <h1 className={styles.postH1} onClick={() => (window.open(article.url))}>{article.title}</h1>
                         <p className={styles.postP}>{article.description}</p>
                         {!!article.urlToImage && <img src={article.urlToImage} />}
                     </div>
